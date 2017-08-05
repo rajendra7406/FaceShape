72ad7b2e7e9ae6ab23e7b9b486ea0ce693bacb52
rajendra7406 <rajendra7406@gmail.com>
2017-08-05 19:31:38 +0530

    98% done
    1. Removed canny edge detection - was not useful
    2. Detected 4 lines required for shape determination
    3. Completed K cluster for detecting forehead length
    4. Added rules to determine shape

--- faceshape.py
+++ faceshape.py
@@ -4,14 +4,14 @@ import cv2
 import dlib
 from sklearn.cluster import KMeans
 
-imagepath = "D:\workspace\FaceShape\i1.jpg"
+imagepath = "D:\workspace\FaceShape\di2.jpg"
 # link = https://github.com/opencv/opencv/tree/master/data/haarcascades
-cascade_path = "D:\workspace\FaceShape\haarcascade_frontalface_default.xml"
+face_cascade_path = "D:\workspace\FaceShape\haarcascade_frontalface_default.xml"
 # download file path = http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
 predictor_path = "D:\workspace\FaceShape\shape_predictor_68_face_landmarks.dat"
 
-#create the haar cascade for detecting face
-faceCascade = cv2.CascadeClassifier(cascade_path)
+#create the haar cascade for detecting face and smile
+faceCascade = cv2.CascadeClassifier(face_cascade_path)
 
 #create the landmark predictor
 predictor = dlib.shape_predictor(predictor_path)
@@ -23,6 +23,8 @@ image = cv2.imread(imagepath)
 v = np.median(image)
 #resizing the image to 100 cols nd 50 rows
 image = cv2.resize(image, (500, 500)) 
+#making another copy
+original = image.copy()
 
 #convert the image to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -38,21 +40,42 @@ faces = faceCascade.detectMultiScale(
     minSize=(100,100),
     flags=cv2.CASCADE_SCALE_IMAGE
     )
-
+#Detect faces in the image
 print("found {0} faces!".format(len(faces)) )
 
-#looping through no of faces detected
 for (x,y,w,h) in faces:
-    #drawing 25% rectangle
-    cv2.rectangle(image,(x,y), (x+w,y+int(0.25*h)), (0,255,100), 2 )
-    #getting area of interest from image i.e., forehead
-    forehead = image[y:y+int(0.25*h), x:x+w]
+    #draw a rectangle around the faces
+    cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 2)
+    #converting the opencv rectangle coordinates to Dlib rectangle
+    dlib_rect = dlib.rectangle(int(x), int(y), int(x+w), int(y+h))
+    #detecting landmarks
+    detected_landmarks = predictor(image, dlib_rect).parts()
+    #converting to np matrix
+    landmarks = np.matrix([[p.x,p.y] for p in detected_landmarks])
+    """
+    #copying the image so we can we side by side
+    landmark = image.copy()
+        for idx, point in enumerate(landmarks):
+            pos = (point[0,0], point[0,1] )
+            #annotate the positions
+            cv2.putText(landmark,str(idx),pos,fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=0.4,color=(0,0,255) )
+            #draw points on the landmark positions 
+            cv2.circle(landmark, pos, 3, color=(0,255,255))
+    
+cv2.imshow("Landmarks by DLib", landmark)
+"""
+#making another copy  for showing final results
+results = original.copy()
 
-    cv2.imshow("forehead",forehead)
+for (x,y,w,h) in faces:
+    #draw a rectangle around the faces
+    cv2.rectangle(results, (x,y), (x+w,y+h), (0,255,0), 2)
+    #getting area of interest from image i.e., forehead (25% of face)
+    temp = original.copy()
+    forehead = temp[y:y+int(0.25*h), x:x+w]
     rows,cols, bands = forehead.shape
-    print('rows=',rows,'cols=',cols,'bands=',bands)
     X = forehead.reshape(rows*cols,bands)
-    #kmeans.
+    #kmeans
     kmeans = KMeans(n_clusters=2,init='k-means++',max_iter=300,n_init=10, random_state=0)
     y_kmeans = kmeans.fit_predict(X)
     for i in range(0,rows):
@@ -61,38 +84,88 @@ for (x,y,w,h) in faces:
                 forehead[i][j]=[255,255,255]
             if y_kmeans[i*cols+j]==False:
                 forehead[i][j]=[0,0,0]
-                cv2.imshow('after',forehead)        
-    #applying canny edge detection 
-    #for more http://www.pyimagesearch.com/2015/04/06/zero-parameter-automatic-canny-edge-detection-with-python-and-opencv/
-    sigma = 0.33 #suggested value
-    #compute the median of the single channel pixel intensities
-    #v = np.median(image)
-    #applying automatic canny edge detection using the computed median 
-    lower = int(max(0,(1.0-sigma)*v))
-    upper = int(min(255,(1.0+sigma)*v))
-    edged = cv2.Canny(forehead,lower,upper)
-    cv2.imshow("auto canny detection", edged)
-    #draw a rectangle around the faces
-    cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 2)
-    #converting the opencv rectangle coordinates to Dlib rectangle
-    dlib_rect = dlib.rectangle(int(x), int(y), int(x+w), int(y+h))
-    print(dlib_rect)
-    #detecting landmarks
-    detected_landmarks = predictor(image, dlib_rect).parts()
-    #converting to np matrix
-    landmarks = np.matrix([[p.x,p.y] for p in detected_landmarks])
-    #copying the image so we can we side by side
-    original = image.copy()
-    for idx, point in enumerate(landmarks):
-            pos = (point[0,0], point[0,1] )
-            #annotate the positions
-            cv2.putText(original,str(idx),pos,fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=0.4,color=(0,0,255) )
-            #draw points on the landmark positions 
-            cv2.circle(original, pos, 3, color=(0,255,255))
+    #getting the length of forehead
+    #1.take midpoint of the forehead
+    #2.travel left and right
+    #3.Consider the point which has change in pixel value
+    forehead_mid = [int(cols/2), int(rows/2) ]
+    lef=0
+    pixel_value = forehead[forehead_mid[1],forehead_mid[0] ]
+    for i in range(0,cols):
+        if forehead[forehead_mid[1],forehead_mid[0]-i].all()!=pixel_value.all():
+            lef=forehead_mid[0]-i
+            break;
+    left = [lef,forehead_mid[1]]
+    rig=0
+    for i in range(0,cols):
+        if forehead[forehead_mid[1],forehead_mid[0]+i].all()!=pixel_value.all():
+            rig = forehead_mid[0]+i
+            break;
+    right = [rig,forehead_mid[1]]
+    
+#drawing line1 on forehead with circles
+line1 = np.subtract(right+y,left+x)[0]
+cv2.line(results, tuple(x+left), tuple(y+right), color=(0,255,0), thickness = 2)
+cv2.putText(results,' Line 1',tuple(x+left),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=1,color=(0,255,0), thickness=2)
+cv2.circle(results, tuple(x+left), 5, color=(255,0,0), thickness=-1)    
+cv2.circle(results, tuple(y+right), 5, color=(255,0,0), thickness=-1)        
 
-cv2.imshow("Faces found", image)
-cv2.imshow("Landmarks found", original)
+#drawing line 2 with circles
+linepointleft = (landmarks[1,0],landmarks[1,1])
+linepointright = (landmarks[15,0],landmarks[15,1])
+line2 = np.subtract(linepointright,linepointleft)[0]
+cv2.line(results, linepointleft,linepointright,color=(0,255,0), thickness = 2)
+cv2.putText(results,' Line 2',linepointleft,fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=1,color=(0,255,0), thickness=2)
+cv2.circle(results, linepointleft, 5, color=(255,0,0), thickness=-1)    
+cv2.circle(results, linepointright, 5, color=(255,0,0), thickness=-1)    
 
+#drawing line 3 with circles
+linepointleft = (landmarks[3,0],landmarks[3,1])
+linepointright = (landmarks[13,0],landmarks[13,1])
+line3 = np.subtract(linepointright,linepointleft)[0]
+cv2.line(results, linepointleft,linepointright,color=(0,255,0), thickness = 2)
+cv2.putText(results,' Line 3',linepointleft,fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=1,color=(0,255,0), thickness=2)
+cv2.circle(results, linepointleft, 5, color=(255,0,0), thickness=-1)    
+cv2.circle(results, linepointright, 5, color=(255,0,0), thickness=-1)    
 
+#drawing line 4 with circles
+linepointbottom = (landmarks[8,0],landmarks[8,1])
+linepointtop = (landmarks[8,0],y)
+line4 = np.subtract(linepointbottom,linepointtop)[1]
+cv2.line(results,linepointtop,linepointbottom,color=(0,255,0), thickness = 2)
+cv2.putText(results,' Line 4',linepointbottom,fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=1,color=(0,255,0), thickness=2)
+cv2.circle(results, linepointtop, 5, color=(255,0,0), thickness=-1)    
+cv2.circle(results, linepointbottom, 5, color=(255,0,0), thickness=-1)    
+print(line1,line2,line3,line4)
 
+maxindex = np.argmax([line1,line2, line3,line4])
+similarity = np.std([line1,line2,line3])
+angle = 30
+for i in range(1):
+  if similarity<10:
+    if angle>40:
+      print('squared')
+      break
+    else:
+      print('round')
+      break
+  if line3>line1:
+    print('triangle shape') 
+    break
+  if maxindex+1 == 2:
+    print('diamond shape')
+    break
+  if line4 > line2:
+    if angle > 40:
+      print('rectangular ')
+      break;
+    else:
+      print('oblong')
+      break;
+   
+    
+  
+  
 
+output = np.concatenate((original,results), axis=1)
+cv2.imshow('output',output)
